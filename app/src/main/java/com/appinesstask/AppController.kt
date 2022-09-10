5ff133180a145b6235f97e601434fd957f2f0391
Manish Mishra <mishramanish426@gmail.com>
2022-09-10 17:52:45 +0530

    Implement appcontroller class for Hilt In Main Activity Class define the binding and attached the view Model instance variable with layout

---
+++ app/src/main/java/com/appinesstask/AppController.kt
@@ -0,0 +1,9 @@
+package com.appinesstask
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppController : Application() {
+
+}
--- app/src/main/java/com/appinesstask/MainActivity.kt
+++ app/src/main/java/com/appinesstask/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.appinesstask
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.appinesstask.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+    private val mainVM by viewModels<MainVM>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainBinding.vm = mainVM
     }
 
 }
